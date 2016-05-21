f65894d7988dcfaece254b7cb877920e8fc0dd32
treyzania <treyzania@gmail.com>
2016-05-21 00:46:56 -0400

    Set up basic networking and stuff.

---
+++ Assets/Code/PlayerScriptEnabler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Vehicles.Car;
+using System.Collections;
+
+public class PlayerScriptEnabler : MonoBehaviour {
+	
+	void Start () {
+		
+		NetworkIdentity ni = this.GetComponent<NetworkIdentity>();
+		bool state = ni.isLocalPlayer;
+		
+		this.GetComponent<CarController>().enabled = state;
+		this.GetComponent<CarUserControl>().enabled = state;
+		this.GetComponent<CarAudio>().enabled = state;
+		
+		Camera cam = this.GetComponentInChildren<Camera>();
+		cam.enabled = state;
+		cam.gameObject.GetComponent<AudioListener>().enabled = state;
+		if (state) Camera.SetupCurrent(cam);
+		
+	}
+	
+}
---
+++ Assets/Code/RacingNetworkManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class RacingNetworkManager : NetworkManager {
+	
+	public string playerSpawnTransformName;
+	
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId) {
+		
+		Vector3 spawnPos = GameObject.Find(this.playerSpawnTransformName).transform.position;
+		
+		GameObject player = (GameObject) GameObject.Instantiate(this.playerPrefab, spawnPos, Quaternion.identity);
+		NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+		
+	}
+	
+}
